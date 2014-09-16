87340613293931882c3b1a8ae04caab73b0bbcc1
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-16 18:49:29 -0400

    Fetching issue title's  via GET request.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('The Iron Yard Rocks');
+function renderTemplate(templateId, model, container) {
+  var templateString = $(templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderTemplate = templateFunction(model);
+  $(container).append(renderTemplate);
+};
+
+var issue = [
+  {issue: "Please comment with the link to your repo, then close issue."},
+  {issue: "Stop making weird issues for no reason"},
+  {issue: "The colors are off on your monitor. Go buy a new monitor."}
+];
+
+
+
+
+
+_.each(issue, function(issue){
+  renderTemplate('#issues_template', issue, '.issues_container')
+});
+
+$.ajax({
+  type: 'GET',
+  url: "https://api.github.com/issues",
+  success: function(item){
+    _.each(item, function(title){
+  renderTemplate('#issues_titles', title, '.titles_container')
+});
+  }
+})
+ 
