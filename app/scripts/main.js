0c001c8ee03cc077dffdd95e8deaca41870f1fb5
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-16 19:37:30 -0400

    Fetching issue body test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,27 +5,30 @@ function renderTemplate(templateId, model, container) {
   $(container).append(renderTemplate);
 };
 
-var issue = [
-  {issue: "Please comment with the link to your repo, then close issue."},
-  {issue: "Stop making weird issues for no reason"},
-  {issue: "The colors are off on your monitor. Go buy a new monitor."}
-];
+// var issue = [
+//   {issue: "Please comment with the link to your repo, then close issue."},
+//   {issue: "Stop making weird issues for no reason"},
+//   {issue: "The colors are off on your monitor. Go buy a new monitor."}
+// ];
 
 
 
 
 
-_.each(issue, function(issue){
-  renderTemplate('#issues_template', issue, '.issues_container')
-});
+// _.each(issue, function(issue){
+//   // renderTemplate('#issues_template', issue, '.issues_container')
+// });
 
 $.ajax({
   type: 'GET',
   url: "https://api.github.com/issues",
-  success: function(item){
-    _.each(item, function(title){
-  renderTemplate('#issues_titles', title, '.titles_container')
-});
+    success: function(issues){
+      console.log(issues);
+      _.each(issues, function(issue){
+        console.log(issue)
+        renderTemplate('#issues_titles', issue, '.titles_container')
+        renderTemplate('#issues_template', issue, '.issues_container')
+    });
   }
 })
  
